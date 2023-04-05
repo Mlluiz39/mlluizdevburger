1bb7dc786c7d9317d7294c19cfefd67e8e9c184d
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-04-05 20:08:11 -0300

    add new code

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,30 @@
 import axios from 'axios'
 
 const apiDevBurger = axios.create({
-	baseURL: 'https://apinodeburger.fly.dev/'
+  baseURL: 'https://apinodeburger.fly.dev/'
 })
 
 apiDevBurger.interceptors.request.use(async (config) => {
-	const userData = await localStorage.getItem('mlluizDevBurger:user')
-	const token = userData && JSON.parse(userData).token
-	config.headers.Authorization = `Bearer ${token}`
-	return config
+  try {
+    const userData = localStorage.getItem('mlluizDevBurger:user')
+    if (!userData) {
+
+      return config
+    }
+    const { token } = JSON.parse(userData)
+    if (!token) {
+
+      return config
+    }
+
+    config.headers.Authorization = `Bearer ${token}`
+    return config
+  } catch (error) {
+
+    console.error(error)
+
+    return config
+  }
 })
 
 export default apiDevBurger
