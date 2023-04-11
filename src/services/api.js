3e4c823299f3ab70e0d5e870c658433fa6e581e0
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-04-11 19:05:10 -0300

    add new api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiDevBurger = axios.create({
-  baseURL: 'https://apinodeburger.fly.dev/'
+  baseURL: 'https://apidevburger.onrender.com/'
 })
 
 apiDevBurger.interceptors.request.use(async (config) => {
